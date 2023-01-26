08474a1e3147400858cd15c39fd4411160a618ba
David Vo <auscompgeek@users.noreply.github.com>
2023-01-26 23:17:46 +1100

    PhotonPoseEstimator: Move an lvalue reference

---
+++ tests/test_pose_estimator.py
@@ -0,0 +1,46 @@
+import robotpy_apriltag
+from wpimath.geometry import Transform3d
+
+import photonvision
+
+
+def test_photon_pose_estimator_ctor():
+    camera = photonvision.PhotonCamera("test_ctor")
+    assert camera._path
+    estimator = photonvision.PhotonPoseEstimator(
+        robotpy_apriltag.loadAprilTagLayoutField(
+            robotpy_apriltag.AprilTagField.k2023ChargedUp
+        ),
+        photonvision.PoseStrategy.AVERAGE_BEST_TARGETS,
+        camera,
+        Transform3d(),
+    )
+    assert estimator
+    assert not camera._path
+
+
+def test_photon_pose_estimator_update():
+    estimator = photonvision.PhotonPoseEstimator(
+        robotpy_apriltag.loadAprilTagLayoutField(
+            robotpy_apriltag.AprilTagField.k2023ChargedUp
+        ),
+        photonvision.PoseStrategy.AVERAGE_BEST_TARGETS,
+        photonvision.PhotonCamera("test_update"),
+        Transform3d(),
+    )
+    assert estimator.update() is None
+
+
+def test_photon_pose_estimator_get_cam():
+    camera = photonvision.PhotonCamera("test_get_cam")
+    estimator = photonvision.PhotonPoseEstimator(
+        robotpy_apriltag.loadAprilTagLayoutField(
+            robotpy_apriltag.AprilTagField.k2023ChargedUp
+        ),
+        photonvision.PoseStrategy.AVERAGE_BEST_TARGETS,
+        camera,
+        Transform3d(),
+    )
+    inner_cam = estimator.getCamera()
+    assert inner_cam is not None
+    assert inner_cam is not camera
